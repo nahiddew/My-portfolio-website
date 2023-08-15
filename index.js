c9e4bf8093d55e417eedee262f669931b3bed0bc
dew <nahiddew1987@gmail.com>
2023-08-16 01:03:20 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+   
+   
+
+
+// const titleLinks=document.querySelectorAll(".title-links");
+// const details=document.querySelectorAll(".details");
+
+// for(let titlelink of titleLinks){
+//     titlelink.addEventListener("click",(e)=>{
+//     //   console.log("clicked");
+//     document.querySelector(".active-link")?.classList.remove("active-link");
+//     titlelink.classList.add('active-link');
+//     for(let detail of details){
+//         document.querySelector(".active")?.classList.remove("active"); 
+//         console.log(e.target);
+     
+      
+      
+//     //    if(detail.classList.contains("active")){
+//     //     detail.classList.remove("active");
+//     //    }
+       
+//       }
+//     })
+   
+// };
+
